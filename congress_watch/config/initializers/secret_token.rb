4d110a80459bcc8d97da3f693aecb3fbb4223293
Dan Crews <dancrews@me.com>
2013-10-20 18:43:45 -0400

    Initial commit. Edited database .yml.

---
+++ congress_watch/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CongressWatch::Application.config.secret_key_base = '0dcc5fb5dd33bf485939aed07d7c71047c2554efc0b3c3c873b56acc15d2fc97f2dd5ca0142828f5f95b4c79e83ed995640d13ac277fd4f9651aa9a012897a29'

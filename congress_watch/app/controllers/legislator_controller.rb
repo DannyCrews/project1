4ead86295cce7322047929bb35ddbbf8bbd1ebb3
Dan Crews <dancrews@me.com>
2013-10-23 18:47:15 -0400

    deleting files that I renamed earlier

--- congress_watch/app/controllers/legislator_controller.rb
+++
@@ -1,9 +0,0 @@
-
-
-class LegislatorsController < ApplicationController
-  def index
-          @user = User.first
-          @zipcode = params[:zipcode]
-          @legislators = Congress.legislators_locate(@zipcode)
-  end
-end

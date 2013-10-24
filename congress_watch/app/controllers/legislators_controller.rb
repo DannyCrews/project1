0fa3b33710be5744ed580db8fe93fda2f777847c
Dan Crews <dancrews@me.com>
2013-10-24 14:04:25 -0400

    routed root to legislators#index. Unpacked resulting array in legislators view.

--- congress_watch/app/controllers/legislators_controller.rb
+++ congress_watch/app/controllers/legislators_controller.rb
@@ -1,9 +1,15 @@
 
 
-# class LegislatorsController < ApplicationController
-#   def index
-#           @user = User.first
-#           @zipcode = params[:zipcode]
-#           @legislators = Congress.legislators_locate(@zipcode)
-#   end
-# end
+class LegislatorsController < ApplicationController
+  def index
+          @user = User.first
+          @zipcode = params[:zipcode]
+          @legislators = Congress.legislators_locate('01879')
+  end
+
+  def create
+        @house_rep = @legislators[:representative]
+        @senator_sr = @legislators[:senior_senator]
+        @senator_jr = @legislators[:junior_senator]
+    end
+end

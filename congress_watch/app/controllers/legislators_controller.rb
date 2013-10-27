8e59b712207687f5244f543e4d46bd9bb7ae52fa
Dan Crews <dancrews@me.com>
2013-10-27 10:04:26 -0400

    added yaml_db and legislators_current

--- congress_watch/app/controllers/legislators_controller.rb
+++ congress_watch/app/controllers/legislators_controller.rb
@@ -1,15 +1,13 @@
-
-
 class LegislatorsController < ApplicationController
   def index
-          @user = User.first
-          @zipcode = params[:zipcode]
-          @legislators = Congress.legislators_locate('01879')
+    @user = User.first
+    @zipcode = params[:zipcode]
+    @legislators = Congress.legislators_locate(@zipcode)
   end
 
   def create
-        @house_rep = @legislators[:representative]
-        @senator_sr = @legislators[:senior_senator]
-        @senator_jr = @legislators[:junior_senator]
-    end
+    @house_rep = @legislators[:representative]
+    @senator_sr = @legislators[:senior_senator]
+    @senator_jr = @legislators[:junior_senator]
+  end
 end
